4b0c88d1f31d5d725e831c3988085305820708d5
Jeremy5909 <jeremyherczeg@gmail.com>
2024-08-03 13:51:23 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## DSCRD
+dscrd is a Discord CLI
+## Hosting locally
+Currently it doesnt work yet but to install to like code on it is instructions below
+### Clone and cd into the repository
+`git clone https://github.com/Jeremy5909/Dscrd.git && cd Dscrd`
+### Install the neccessary packages
+`npm i`
+### Setup dotenv secrets
+`touch .env`
+Fill this in
+```
+ClientID=..
+ClientSecret=..
+PORT=8080
+token=..
+```
+## Installation
+??
---
+++ src/index.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const url = require('url');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.get('/api/auth/discord/redirect', async (req,_) => {
+    const { code } = req.query;
+
+    if (code) {
+
+        // Exchange code for token & refresh token
+        const formData = new url.URLSearchParams({
+            client_id: process.env.ClientID,
+            client_secret: process.env.ClientSecret,
+            grant_type: 'authorization_code',
+            code: code.toString(),
+            redirect_uri: 'http://localhost:8080/api/auth/discord/redirect'
+        });
+
+        const output = await axios.post('https://discord.com/api/v10/oauth2/token', 
+            formData, {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+        });
+
+        if (output.data) {
+            const access = output.data.access_token;
+            const userinfo = await axios.get('https://discord.com/api/v10/users/@me', {
+                headers: {
+                    'Authorization': `Bearer ${access}`
+                },
+            });
+
+            // Refresh token
+            const formData1 = new url.URLSearchParams({
+                client_id: process.env.ClientID,
+                client_secret: process.env.ClientSecret,
+                grant_type: 'refresh_token',
+                refresh_token: output.data.refresh_token
+            });
+
+            const refresh = await axios.post('https://discord.com/api/v10/oauth2/token', 
+                formData1, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+            });
+            console.log(output.data, userinfo.data, refresh.data)
+        }
+    }
+});
+
+app.listen(port, () => {console.log(`Running on ${port}`)} )
